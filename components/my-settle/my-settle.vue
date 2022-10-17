eccdf7d177ac8a896a0cd3085d0da328a5da3705
hnwj <2948662591@qq.com>
2022-10-17 09:48:58 +0800

    完成了登录和支付功能的开发

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -12,23 +12,30 @@
 		</view>
 
 		<!-- 结算按钮 -->
-		<view class="btn-settle">结算({{checkedCount}})</view>
+		<view class="btn-settle" @click="settlement">结算({{checkedCount}})</view>
 	</view>
 </template>
 
 <script>
 	import {
 		mapGetters,
-		mapMutations
+		mapMutations,
+		mapState
 	} from 'vuex'
 	export default {
 		data() {
 			return {
-
+				// 倒计时的秒数
+				seconds: 3,
+				// 定时器的 Id
+				timer: null
 			}
 		},
 		computed: {
 			...mapGetters('m_cart', ['checkedCount', 'total', , 'checkedGoodsAmount']),
+			...mapGetters('m_user', ['addstr']),
+			...mapState('m_user', ['token']),
+			...mapState('m_cart', ['cart']),
 			// 是否全选
 			isFullCheck() {
 				return this.total === this.checkedCount
@@ -36,9 +43,104 @@
 		},
 		methods: {
 			...mapMutations('m_cart', ['updateAllGoodsState']),
+			...mapMutations('m_user', ['updateRedirectInfo']),
 			// 实现全选或反选
 			changeAllState() {
 				this.updateAllGoodsState(!this.isFullCheck)
+			},
+			// 用户点击结算按钮
+			settlement() {
+				// 判断用户是否勾选了商品
+				if (!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
+				// 2. 再判断用户是否选择了收货地址
+				if (!this.addstr) return uni.$showMsg('请选择收货地址！')
+				// 3. 最后判断用户是否登录了
+				// if (!this.token) return uni.$showMsg('请先登录！')
+				if (!this.token) return this.delayNavigate()
+				// 4. 实现微信支付功能
+				this.payOrder()
+			},
+			// 创建订单
+			async payOrder() {
+				// 1.1 组织订单的信息对象
+				const orderInfo = {
+					// 开发期间，注释掉真实的订单价格，
+					// order_price: this.checkedGoodsAmount,
+					// 写死订单总价为 1 分钱
+					order_price: 0.01,
+					consignee_addr: this.addstr,
+					goods: this.cart.filter(x => x.goods_state).map(x => ({
+						goods_id: x.goods_id,
+						goods_number: x.goods_count,
+						goods_price: x.goods_price
+					}))
+				}
+				// 1.2 发起请求创建订单
+				const {
+					data: res
+				} = await uni.$http.post('/api/public/v1/my/orders/create', orderInfo)
+				if (res.meta.status !== 200) return uni.$showMsg('创建订单失败！')
+				// 1.3 得到服务器响应的“订单编号”
+				const orderNumber = res.message.order_number
+				// 2. 订单预支付
+				const {
+					data: res2
+				} = await uni.$http.post('/api/public/v1/my/orders/req_unifiedorder', {
+					order_number: orderNumber
+				})
+				if (res2.meta.status !== 200) return uni.$showError('预付订单生成失败！')
+				const payInfo = res2.message.pay
+				// 3. 发起微信支付
+				const [err, succ] = await uni.requestPayment(payInfo)
+				if (err) return uni.$showMsg('订单未支付！')
+				const {
+					data: res3
+				} = await uni.$http.post('/api/public/v1/my/orders/chkOrder', {
+					order_number: orderNumber
+				})
+				if (res3.meta.status !== 200) return uni.$showMsg('订单未支付！')
+				// 3.5 检测到订单支付完成
+				uni.showToast({
+					title: '支付完成！',
+					icon: 'success'
+				})
+			},
+			// 展示倒计时的提示消息
+			showTips(n) {
+				uni.showToast({
+					icon: 'none',
+					title: '请登录后再结算！' + n + ' 秒后自动跳转到登录页',
+					mask: true,
+					duration: 1500
+				})
+			},
+			// 延迟导航到 my 页面
+			delayNavigate() {
+				this.seconds = 3
+				this.showTips(this.seconds)
+				this.timer = setInterval(() => {
+					this.seconds--
+					if (this.seconds <= 0) {
+						// 2.1 清除定时器
+						clearInterval(this.timer)
+						// 2.2 跳转到 my 页面
+						uni.switchTab({
+							url: '/pages/my/my',
+							// 页面跳转成功之后的回调函数
+							success: () => {
+								// 调用 vuex 的 updateRedirectInfo 方法，把跳转信息存储到 Store 中
+								this.updateRedirectInfo({
+									// 跳转的方式
+									openType: 'switchTab',
+									// 从哪个页面跳转过去的
+									from: '/pages/cart/cart'
+								})
+							}
+						})
+						return
+					}
+					this.showTips(this.seconds)
+				}, 1000)
 			}
 		}
 	}
